c79f80eaceb30c041e6303a75dfe5aba244d3352
TMaysGGS <tmaysggs@gmail.com>
2019-11-22 09:59:22 +0800

    Modify MTCNN_models.py

--- MTCNN_models.py
+++ MTCNN_models.py
@@ -5,12 +5,12 @@ Created on Wed Aug  7 09:59:45 2019
 @author: TMaysGGS
 """
 
-'''Last updated on 10/08/2019 01:31'''
+'''Last updated on 11/17/2019 13:46'''
 from tensorflow.keras.layers import Input, Conv2D, PReLU, MaxPooling2D, Reshape, Concatenate, Flatten, Dense
 from tensorflow.keras.models import Model
 
 '''P-Net'''
-def pnet(training = False):
+def pnet(training = False, train_with_landmark = False):
     
     if training:
         X = Input(shape = (12, 12, 3), name = 'Pnet_input')
@@ -33,49 +33,21 @@ def pnet(training = False):
     
     if training:
         Classifier = Reshape((1, ), name = 'Pnet_classifier')(Classifier_conv)
-        Bbox_regressor = Reshape((4, ), name = 'Pnet_bbox_regressor')(Bbox_regressor_conv)
-        Landmark_regressor = Reshape((12, ), name = 'Pnet_landmark_regressor')(Landmark_regressor_conv)
-        Pnet_output = Concatenate()([Classifier, Bbox_regressor, Landmark_regressor])
-        
-        model = Model(X, Pnet_output)
+        Bbox_regressor = Reshape((4, ), name = 'Pnet_bbox_regressor')(Bbox_regressor_conv) 
+        if train_with_landmark: 
+            Landmark_regressor = Reshape((12, ), name = 'Pnet_landmark_regressor')(Landmark_regressor_conv)
+            Pnet_output = Concatenate()([Classifier, Bbox_regressor, Landmark_regressor]) 
+            model = Model(X, Pnet_output) 
+        else:
+            Pnet_output = Concatenate()([Classifier, Bbox_regressor])
+            model = Model(X, Pnet_output)         
     else:
         model = Model(X, [Classifier_conv, Bbox_regressor_conv])
     
     return model
 
-#def pnet(training = False):
-#    
-#    if training:
-#        X = Input(shape = (12, 12, 3), name = 'Pnet_input')
-#    else:
-#        X = Input(shape = (None, None, 3), name = 'Pnet_input')
-#    
-#    M = Conv2D(10, 3, strides = 1, padding = 'valid', name = 'Pnet_conv1')(X)
-#    M = PReLU(shared_axes = [1, 2], name = 'Pnet_prelu1')(M)
-#    M = MaxPooling2D(pool_size = 2, name = 'Pnet_maxpool1')(M) # default 'pool_size' is 2!!! 
-#    
-#    M = Conv2D(16, 3, strides = 1, padding = 'valid', name = 'Pnet_conv2')(M)
-#    M = PReLU(shared_axes= [1, 2], name = 'Pnet_prelu2')(M)
-#    
-#    M = Conv2D(32, 3, strides = 1, padding = 'valid', name = 'Pnet_conv3')(M)
-#    M = PReLU(shared_axes= [1, 2], name = 'Pnet_prelu3')(M)
-#    
-#    Classifier_conv = Conv2D(1, 1, activation = 'sigmoid', name = 'Pnet_classifier_conv')(M)
-#    Bbox_regressor_conv = Conv2D(4, 1, name = 'Pnet_bbox_regressor_conv')(M)
-#    
-#    if training:
-#        Classifier = Reshape((1, ), name = 'Pnet_classifier')(Classifier_conv)
-#        Bbox_regressor = Reshape((4, ), name = 'Pnet_bbox_regressor')(Bbox_regressor_conv)
-#        Pnet_output = Concatenate()([Classifier, Bbox_regressor])
-#        
-#        model = Model(X, Pnet_output)
-#    else:
-#        model = Model(X, [Classifier_conv, Bbox_regressor_conv])
-#    
-#    return model
-
 '''R-Net'''
-def rnet(training = False):
+def rnet(training = False, train_with_landmark = False):
     
     X = Input(shape = (24, 24, 3), name = 'Rnet_input')
     
@@ -94,13 +66,15 @@ def rnet(training = False):
     M = Dense(128, name = 'Rnet_fc')(M)
     M = PReLU(name = 'Rnet_prelu4')(M)
     
-    Classifier = Dense(2, activation = 'softmax', name = 'Rnet_classifier')(M)
+    Classifier = Dense(1, activation = 'sigmoid', name = 'Rnet_classifier')(M)
     Bbox_regressor = Dense(4, name = 'Rnet_bbox_regressor')(M)
-    Landmark_regressor = Dense(12, name = 'Rnet_landmark_regressor')(M)
     
-    if training:
-        Rnet_output = Concatenate()([Classifier, Bbox_regressor, Landmark_regressor])
-        
+    if training and train_with_landmark: 
+        Landmark_regressor = Dense(12, name = 'Rnet_landmark_regressor')(M)
+        Rnet_output = Concatenate()([Classifier, Bbox_regressor, Landmark_regressor]) 
+        model = Model(X, Rnet_output) 
+    elif training and not train_with_landmark: 
+        Rnet_output = Concatenate()([Classifier, Bbox_regressor, Landmark_regressor]) 
         model = Model(X, Rnet_output)
     else:
         model = Model(X, [Classifier, Bbox_regressor])
